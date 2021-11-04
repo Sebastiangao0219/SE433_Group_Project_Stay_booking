3c3991771405069cc621d4c82a35b603f7bd63bc
Justin Zhang <jingyu120@gmail.com>
2021-11-04 13:34:58 -0500

    added student controller

---
+++ src/main/java/com/depaul/se433_project_stay_booking/controller/Gender.java
@@ -0,0 +1,7 @@
+package com.depaul.se433_project_stay_booking.controller;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/main/java/com/depaul/se433_project_stay_booking/controller/Student.java
@@ -0,0 +1,16 @@
+package com.depaul.se433_project_stay_booking.controller;
+
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private Gender gender;
+}
